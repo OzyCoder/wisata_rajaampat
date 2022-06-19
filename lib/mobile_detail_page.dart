e85e539a9c467f8399ee3b2748dbb0457ffa2c68
ozydev <fauzirramadan@gmail.com>
2022-06-19 14:50:01 +0700

    revisi pertama

--- lib/mobile_detail_page.dart
+++ lib/mobile_detail_page.dart
@@ -1,27 +1,12 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wisata_rajaampat/model/tourism_place.dart';
-import 'package:wisata_rajaampat/mobile_detail_page.dart';
+import 'detail_screen.dart';
 
-final buttonFontStyle = GoogleFonts.mukta(
-    fontStyle: FontStyle.normal, fontSize: 20, fontWeight: FontWeight.bold);
-
-final textStyle = GoogleFonts.poppins(
-  color: Colors.blueGrey,
-  fontStyle: FontStyle.normal,
-  fontWeight: FontWeight.w500,
-  fontSize: 14,
-);
-
-final muktaFontStyle =
-    GoogleFonts.mukta(fontStyle: FontStyle.italic, fontSize: 17);
-
-class DetailScreen extends StatelessWidget {
+class MobileDetailScreen extends StatelessWidget {
   final TourismPlace tempat;
 
-  const DetailScreen({Key? key, required this.tempat}) : super(key: key);
+  const MobileDetailScreen({Key? key, required this.tempat}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -159,60 +144,5 @@ class DetailScreen extends StatelessWidget {
         ],
       ),
     ));
-  }
-}
-
-class FavoriteButton extends StatefulWidget {
-  const FavoriteButton({Key? key}) : super(key: key);
-
-  @override
-  State<FavoriteButton> createState() => _FavoriteButtonState();
-}
-
-class _FavoriteButtonState extends State<FavoriteButton> {
-  bool isFavorite = false;
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-        onPressed: () {
-          setState(() {
-            isFavorite = !isFavorite;
-          });
-        },
-        icon: Icon(
-          isFavorite ? Icons.favorite : Icons.favorite_border,
-          color: Colors.red,
-        ));
-  }
-}
-
-class BookmarkButton extends StatefulWidget {
-  const BookmarkButton({Key? key}) : super(key: key);
-
-  @override
-  State<BookmarkButton> createState() => _BookmarkButtonState();
-}
-
-class _BookmarkButtonState extends State<BookmarkButton> {
-  bool isBookmark = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-        onPressed: () {
-          setState(() {
-            isBookmark = !isBookmark;
-          });
-        },
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              isBookmark ? Icons.bookmark : Icons.bookmark_border,
-            ),
-            Text(isBookmark ? " Bookmarked" : " Bookmark",
-                style: buttonFontStyle),
-          ],
-        ));
   }
 }
